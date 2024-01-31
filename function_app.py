8d31ad219771e6132460608f970146dd26e16af7
Juha Tervo <jutervo@outlook.com>
2024-01-31 14:08:33 +0200

    naming

--- function_app.py
+++ function_app.py
@@ -1,9 +1,8 @@
 import logging
 import azure.functions as func
 
-
 app = func.FunctionApp()
 
 @app.event_grid_trigger(arg_name="azeventgrid")
-def EventGridTriggerTrial(azeventgrid: func.EventGridEvent):
+def EventGridTriggerTrialToo(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
